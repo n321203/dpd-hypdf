a3223670f671bdd7875f614a597696563c6edfa5
n321203 <n321203@gmail.com>
2015-06-02 14:49:21 +0200

    5

--- index.js
+++ index.js
@@ -17,36 +17,37 @@ HyPDF.prototype.handle = function(ctx, next){
 	var dpd = internalClient.build(process.server);
 
 	// Only allow POST
-	// if(ctx.req && ctx.req.method !== 'POST') 
-	// 	return next();
+	if(ctx.req && ctx.req.method !== 'POST') 
+		return next();
 
 	// Validate
-	// var body = ctx.req.body || {}
-	// if (!body || !body.k || body.k != DPD_HyPDF_KEY) {
-	//     return next();
-	// }
+	var body = ctx.req.body || {}
+	if (!body || !body.html){
+		return next();
+	}
 
+	var current_date = (new Date()).valueOf().toString();
+	var random = Math.random().toString();
+	var randomfilename = crypto.createHash('sha1').update(current_date + random).digest('hex');
 
-	var user = "app18800740@heroku.com";
-	var pass = "T5cBfEH55GOL";
-
-	//var hypdf = new HypdfLib(process.env.HYPDF_USER, process.env.HYPDF_PASSWORD, {
 	var hypdf = new HypdfLib(user, pass, {
-
 	    // default options to use - these can be changed for each individual API request 
 	    bucket: "bolanegruppenpdf",
 	    public: true, // all S3 uploads will be public by default 
-	    test: true // we are in test mode - these requests won't count against our HyPDF quota 
+	    test: true // we are in test mode - these requests won't count against our HyPDF quota
 	});
 
-	hypdf.htmltopdf("<html><body><h1>Title</h1></body></html>", {
-	        orientation: 'Landscape'
-	        // ... other options ... 
+	hypdf.htmltopdf(body.html, {
+
+			page_size: 	"A4",
+	        orientation:"Portrait",
+
+	        key: randomfilename + ".pdf", 	// Filename
+	        encoding: "UTF-8"			
 	    },
 	    function(err, response) {
 	        if (err) throw err;
-	        console.log(response.pdf);
-	        ctx.done(null, JSON.stringify(response));
+	        ctx.done(null, response);
 	    }
 	);
 }
